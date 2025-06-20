d2f6aaa4564e13df6de71b3f00766a387b309eba
Goshan57 <485262323@mail.ru>
2025-06-20 14:40:44 +0300

    Created auth router

--- shop-app/api/api_v1/__init__.py
+++ shop-app/api/api_v1/__init__.py
@@ -3,10 +3,14 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .products import router as products_router
+from .auth import router as auth_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 router.include_router(
     products_router,
+)
+router.include_router(
+    auth_router,
 )
--- shop-app/core/config.py
+++ shop-app/core/config.py
@@ -14,6 +14,7 @@ class RunConfig(BaseModel):
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
     products: str = "/products"
+    auth: str = "/auth"
 
 
 class ApiPrefix(BaseModel):
