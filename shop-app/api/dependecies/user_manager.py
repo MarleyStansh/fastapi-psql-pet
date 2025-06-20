37f96c0d91e534920446b4a11ffa83034789a958
Goshan57 <485262323@mail.ru>
2025-06-20 14:00:51 +0300

    Created UserManager with dependency

---
+++ shop-app/api/dependecies/user_manager.py
@@ -0,0 +1,10 @@
+from users import get_user_db
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from core.authentication.user_manager import UserManager
+
+
+async def get_user_manager(
+    user_db: SQLAlchemyUserDatabase = Depends(get_user_db),
+):
+    yield UserManager(user_db)
