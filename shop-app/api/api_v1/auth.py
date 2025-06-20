51f90e026616e8f75bcc6213b89d6529242b3a5a
Goshan57 <485262323@mail.ru>
2025-06-20 14:47:47 +0300

    Included fastapi_users router to api_v1

--- shop-app/api/api_v1/auth.py
+++ shop-app/api/api_v1/auth.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
 from core.config import settings
+from .fastapi_users_routers import fastapi_users
+from api.dependecies.authentication import (
+    authentication_backend,
+)
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    fastapi_users.get_auth_router(authentication_backend),
 )
--- shop-app/api/api_v1/fastapi_users_routers.py
+++ shop-app/api/api_v1/fastapi_users_routers.py
@@ -3,8 +3,10 @@ from core.types import UserIdType
 from fastapi_users import FastAPIUsers
 
 from core.models import User
-from .user_manager import get_user_manager
-from .backend import authentication_backend
+from api.dependecies.authentication import (
+    get_user_manager,
+    authentication_backend,
+)
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
