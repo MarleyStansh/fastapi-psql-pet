94f098e3488865152bb8ee48ce2f7275daf3dd5d
Goshan57 <485262323@mail.ru>
2025-06-20 11:15:25 +0300

    Applied migration create-user-table

--- shop-app/core/models/__init__.py
+++ shop-app/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "Product",
+    "User",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .product import Product
+from .user import User
