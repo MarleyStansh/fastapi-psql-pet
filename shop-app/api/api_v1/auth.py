363f21b126017c47470d278e165e2b688a86124e
Goshan57 <485262323@mail.ru>
2025-06-20 15:03:08 +0300

    Added registration router

--- shop-app/api/api_v1/auth.py
+++ shop-app/api/api_v1/auth.py
@@ -4,12 +4,23 @@ from .fastapi_users_routers import fastapi_users
 from api.dependecies.authentication import (
     authentication_backend,
 )
+from core.schemas.user import UserCreate, UserRead
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
 )
 
+# /login/
+# /logout/
 router.include_router(
     fastapi_users.get_auth_router(authentication_backend),
+)
+
+# /register/
+router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
