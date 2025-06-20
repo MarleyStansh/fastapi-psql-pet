081bda8be4d773b055f618039e460b92ade35bb1
Goshan57 <485262323@mail.ru>
2025-06-20 13:57:00 +0300

    Update config: added secrets

--- shop-app/core/config.py
+++ shop-app/core/config.py
@@ -39,6 +39,8 @@ class DatabaseConfig(BaseModel):
 
 class AccessTokenConfig(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
 
 
 class Settings(BaseSettings):
